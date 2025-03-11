ba6e22dd563f730c1cc0674d8221b8085faf550a
Beccatoni <rbccmtni1@gmail.com>
2025-03-11 11:02:13 +0200

    chore: creating variables

--- introductiontoc#/Program.cs
+++ introductiontoc#/Program.cs
@@ -16,6 +16,12 @@ namespace introductiontoc_
             Console.WriteLine("How old are you?");
             string input = Console.ReadLine();
             Console.WriteLine($"You are {input} year old!");  //output the input
+
+            // variables
+            string foo = "Hello";
+            string bar = "How are you doing?";
+            int x = 5;
+            Console.WriteLine(foo);
         }
     }
 }
