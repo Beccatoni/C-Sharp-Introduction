3353edf4faf86800d01778da8d2cc775d067a10a
Beccatoni <rbccmtni1@gmail.com>
2025-03-14 16:02:05 +0200

    calculations with numbers

--- introductiontoc#/Program.cs
+++ introductiontoc#/Program.cs
@@ -138,6 +138,33 @@ namespace introductiontoc_
             Console.WriteLine(pizzaShops);
             Console.WriteLine(totalEmployees);
             Console.WriteLine(revenue2);
+
+
+            //number values
+            // Your Age
+            int userAge = 26;
+
+            // Length of years on Jupiter (in Earth years)
+            double jupiterYears = 11.86;
+
+            // Age on Jupiter
+            double jupiterAge = userAge / jupiterYears;
+
+            // Time to Jupiter
+            double journeyToJupiter = 6.142466;
+
+            // New Age on Earth
+            double newEarthAge = userAge + journeyToJupiter;
+            // New Age on Jupiter
+            double newJupiterAge = newEarthAge / jupiterYears;
+
+            // Log calculations to console
+            Console.WriteLine(userAge);
+            Console.WriteLine(jupiterYears);
+            Console.WriteLine(jupiterAge);
+            Console.WriteLine(journeyToJupiter);
+            Console.WriteLine(newEarthAge);
+            Console.WriteLine(newJupiterAge);
         }
     }
 }
