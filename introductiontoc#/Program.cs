fcfab787838f07cbfe94dff9b27a88f46440ce7f
Beccatoni <rbccmtni1@gmail.com>
2025-03-18 10:41:30 +0200

    Working with text

--- introductiontoc#/Program.cs
+++ introductiontoc#/Program.cs
@@ -191,6 +191,24 @@ namespace introductiontoc_
 
             // Print the lowest number
             Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+
+
+            //working with text
+            string drink = "wine";
+            string madTeaParty = $"\"Have some {drink},\" the March Hare said in an encouraging tone.\nAlice looked" +
+                $" all round the table, but there was nothing on it but tea.\n\"I don't see any {drink},\" she remarked. \n\"There isn't any," +
+                $" \" said the March Hare.";
+
+            int storyLength = madTeaParty.Length;
+            string toFind = "March Hare";
+
+            string findLowerCase = toFind.ToLower();
+            int findMarchHare = madTeaParty.IndexOf(toFind);
+
+            Console.WriteLine(madTeaParty.Substring(findMarchHare));
+            Console.WriteLine($"This scene is {storyLength} long.\n");
+            Console.WriteLine($"The term we're looking for is {toFind} and is located at indec {findMarchHare}.");
+            Console.ReadLine();
         }
     }
 }
